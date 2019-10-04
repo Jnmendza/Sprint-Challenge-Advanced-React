9baa5a7333e44e96b969a9ba1ba5bed90da85c41
Jnmendza <jonatmendoza@gmail.com>
2019-10-04 08:51:50 -0700

    components are setup

---
+++ client/src/components/PlayerCard.js
@@ -0,0 +1,11 @@
+import React from 'react';
+
+const PlayerCard = ({ player, country, search, key }) => {
+
+    return (
+        <div className="playerCard" key={key}>
+        </div>
+    )
+}
+
+export default PlayerCard;
---
+++ client/src/components/PlayerList.js
@@ -0,0 +1,10 @@
+import React from 'react';
+
+const PlayerList = ({ players }) => {
+    return (
+        <div>
+        </div>
+    )
+}
+
+export default PlayerList
