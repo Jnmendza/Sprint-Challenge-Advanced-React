8a81aef10019ce82a44047b6a2ea1bd1ad517a58
Jnmendza <jonatmendoza@gmail.com>
2019-10-04 08:44:18 -0700

    axios setup, receiving data from api

--- client/src/App.js
+++ client/src/App.js
@@ -1,26 +1,37 @@
 import React from 'react';
-import logo from './logo.svg';
 import './App.css';
+import axios from 'axios'
 
-function App() {
+class App extends React.Component {
+  constructor() {
+    super();
+    this.state = {
+      player: []
+    }
+  }
+
+    componentDidMount() {
+      axios
+        .get(`http://localhost:5000/api/players`)
+        .then(response => {
+          console.log(response)
+          let playerData = response.data;
+          this.state({
+            player: playerData
+          })
+        })
+        .catch(err => {
+          console.log(err)
+        })
+    }
+  
+  render() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      Player List goes here
     </div>
-  );
+   );
+  }
 }
 
 export default App;
